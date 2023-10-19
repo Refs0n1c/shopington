f2b97fe744ebc5d9eb49c2533f28ed7721b83d7c
Daniel <daniel.refson2306@gmail.com>
2023-10-19 09:17:59 +0100

    fixed render pug bug

--- routes/shopington.js
+++ routes/shopington.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/',(req,res) => {
     
-    res.render('shop',{prods: data.products, routeConstants: routeConstants, docTitle: 'Shop Ahoy!', path: routeConstants.Shop});
+    res.render('shop',{prods: data.products, routeConstants: routeConstants, pageTitle: 'Shop Ahoy!', path: routeConstants.Shop});
 });
 
 module.exports = router;
